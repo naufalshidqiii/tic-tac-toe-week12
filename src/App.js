2a0ef5dcc1e0f0eedff30738470f3b88128d0eaa
Naufal Shidqi <naufalshidqi99@gmail.com>
2023-03-28 23:31:18 +0700

    belum theming

--- src/App.js
+++ src/App.js
@@ -1,95 +1,152 @@
+import * as React from "react";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { Provider, useDispatch, useSelector } from "react-redux";
 
-import * as React from 'react';
+const ticTacToe = createSlice({
+	name: "ticTacToe",
+	initialState: {
+		squares: Array(9).fill(null),
+		currentMove: 0,
+		winner: null,
+		nextValue: "X",
+		status: "Next player: X",
+	},
+	reducers: {
+		selectSquare(state, action) {
+			if (!state.winner && !state.squares[action.payload]) {
+				const newSquares = [...state.squares];
+				newSquares[action.payload] = calculateNextValue(state.squares);
+				const winner = calculateWinner(newSquares);
+				const nextValue = calculateNextValue(newSquares);
+				const status = calculateStatus(winner, newSquares, nextValue);
 
+				return {
+					squares: newSquares,
+					winner,
+					nextValue,
+					status,
+				};
+			}
+		},
+		restart() {
+			const newSquares = Array(9).fill(null);
+			const winner = calculateWinner(newSquares);
+			const nextValue = calculateNextValue(newSquares);
+			const status = calculateStatus(winner, newSquares, nextValue);
+
+			return {
+				squares: newSquares,
+				winner,
+				nextValue,
+				status,
+			};
+		},
+	},
+});
+
+//Actions
+export const { selectSquare, restart } = ticTacToe.actions;
+
+//Redux Store
+const store = configureStore({
+	reducer: ticTacToe.reducer,
+});
+
+//React Components
 function Board() {
-  const squares = Array(9).fill(null);
-  function selectSquare(square) {
+	const { status, squares } = useSelector((state) => state);
+	const dispatch = useDispatch();
+	function handleSelectSquare(i) {
+		dispatch(selectSquare(i));
+	}
 
-  }
+	function restart() {}
 
-  function restart() {
-  }
+	function renderSquare(i) {
+		return (
+			<button className="square" onClick={() => handleSelectSquare(i)}>
+				{squares[i]}
+			</button>
+		);
+	}
 
-  function renderSquare(i) {
-    return (
-      <button className="square" onClick={() => selectSquare(i)}>
-        {squares[i]}
-      </button>
-    );
-  }
-
-  return (
-    <div>
-      <div >STATUS</div>
-      <div >
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div >
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div >
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
-      <button onClick={restart}>
-        restart
-      </button>
-    </div>
-  );
+	return (
+		<div>
+			<div>{status}</div>
+			<div>
+				{renderSquare(0)}
+				{renderSquare(1)}
+				{renderSquare(2)}
+			</div>
+			<div>
+				{renderSquare(3)}
+				{renderSquare(4)}
+				{renderSquare(5)}
+			</div>
+			<div>
+				{renderSquare(6)}
+				{renderSquare(7)}
+				{renderSquare(8)}
+			</div>
+		</div>
+	);
 }
 
 function Game() {
-  return (
-    <div >
-      <div >
-        <Board />
-      </div>
-    </div>
-  );
+	const dispatch = useDispatch();
+	function handleRestart() {
+		dispatch(restart());
+	}
+
+	return (
+		<div>
+			<div>
+				<Board />
+			</div>
+			<button onClick={handleRestart}>restart</button>
+		</div>
+	);
 }
 
 // eslint-disable-next-line no-unused-vars
 function calculateStatus(winner, squares, nextValue) {
-  return winner
-    ? `Winner: ${winner}`
-    : squares.every(Boolean)
-      ? `Scratch: Cat's game`
-      : `Next player: ${nextValue}`;
+	return winner
+		? `Winner: ${winner}`
+		: squares.every(Boolean)
+		? `Scratch: Cat's game`
+		: `Next player: ${nextValue}`;
 }
 
 // eslint-disable-next-line no-unused-vars
 function calculateNextValue(squares) {
-  return squares.filter(Boolean).length % 2 === 0 ? 'X' : 'O';
+	return squares.filter(Boolean).length % 2 === 0 ? "X" : "O";
 }
 
 // eslint-disable-next-line no-unused-vars
 function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
 }
 
-function App() {
-  return <Game />;
+export default function App() {
+	return (
+		<Provider store={store}>
+			<Game />
+		</Provider>
+	);
 }
-
-export default App;
